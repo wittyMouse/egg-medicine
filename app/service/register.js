e138a5a395580b512c4f26e216107db6551b6c91
wittyMouse <505357658@qq.com>
2019-03-31 12:30:03 +0800

    wittyMouse

--- app/service/register.js
+++ app/service/register.js
@@ -121,14 +121,27 @@ class RegisterService extends Service {
   async registerRecord(params) {
     let data = utils.objectCTUS(params);
     const { token, type, p, page_size } = data;
-    if (!token) {
-      return { msg: 'token不能为空', status: 1 };
+ 
+    let sql = "SELECT f.hospital_name, e.department_name, d.doctor_name, d.doctor_avatar, b.patient_name, c.record_id, c.type, c.fee, c.visit_time FROM ((((wx_token AS a JOIN patient AS b ON a.open_id = b.open_id) JOIN registered_record AS c ON b.patient_id = c.patient_id) JOIN doctor AS d ON c.doctor_id = d.doctor_id) JOIN department AS e ON d.department_id = e.department_id) JOIN hospital AS f ON e.hospital_id = f.hospital_id";
+    let array = [];
+
+    if (token) {
+      if (sql.search(/WHERE/) > -1) {
+        sql += ' AND';
+      } else {
+        sql += ' WHERE';
+      }
+      sql += ' a.token = ?';
+      array.push(token);
     }
-    let sql = "SELECT f.hospital_name, e.department_name, d.doctor_name, d.doctor_avatar, b.patient_name, c.record_id, c.type, c.fee, c.visit_time FROM ((((wx_token AS a JOIN patient AS b ON a.open_id = b.open_id) JOIN registered_record AS c ON b.patient_id = c.patient_id) JOIN doctor AS d ON c.doctor_id = d.doctor_id) JOIN department AS e ON d.department_id = e.department_id) JOIN hospital AS f ON e.hospital_id = f.hospital_id WHERE a.token = ?";
-    let array = [token];
 
     if (type != 0) {
-      sql += " AND c.type = ?";
+      if (sql.search(/WHERE/) > -1) {
+        sql += ' AND';
+      } else {
+        sql += ' WHERE';
+      }
+      sql += ' c.type = ?';
       array.push(type);
     }
 
@@ -146,12 +159,10 @@ class RegisterService extends Service {
   }
 
   async registerDetail(id) {
-    let sql = "SELECT a.record_id AS register_recordId, a.patient_id AS register_patientId, a.doctor_id AS register_doctorId, a.visit_time AS register_visitTime, a.type AS register_type, a.registration_fees AS register_registrationFees, a.create_time AS register_createTime, b.patient_id AS patient_patientId, b.open_id AS patient_openId, b.patient_name AS patient_patientName, b.gender AS patient_gender, b.id_card AS patient_idCard, b.phone_number AS patient_phoneNumber, b.address AS patient_address, b.is_default AS patient_isDefault, b.create_time AS patient_createTime, c.doctor_id AS doctor_doctorId, c.hospital_id AS doctor_hospitalId, c.department_id AS doctor_departmentId, c.doctor_name AS doctor_doctorName, c.doctor_avatar AS doctor_doctorAvatar, c.introduction AS doctor_introduction, c.create_time AS doctor_createTime, d.hospital_id AS hospital_hospitalId, d.hospital_name AS hospital_hospitalName, d.hospital_logo AS hospital_hospitalLogo, d.address AS hospital_address, d.longitude AS hospital_longitude, d.latitude AS hospital_latitude, d.contacts AS hospital_contacts, d.introduction AS hospital_introduction, d.create_time AS hospital_createTime, e.department_id AS department_departmentId, e.hospital_id AS department_hospitalId, e.parent_id AS department_parentId, e.department_name AS department_departmentName, e.introduction AS department_introduction, e.create_time AS department_createTime FROM ((((registered_record AS a LEFT JOIN patient AS b ON a.patient_id = b.patient_id) LEFT JOIN doctor AS c ON a.doctor_id = c.doctor_id) LEFT JOIN hospital AS d ON c.hospital_id = d.hospital_id) LEFT JOIN department AS e ON c.department_id = e.department_id)";
-    sql += ' WHERE a.patient_id IN (?)';
-    const result = await this.app.mysql.query(sql, [id]);
+    let sql = "SELECT f.hospital_name, e.department_name, d.doctor_name, d.doctor_avatar, b.patient_name, c.record_id, c.type, c.fee, c.visit_time, c.create_time FROM ((((wx_token AS a JOIN patient AS b ON a.open_id = b.open_id) JOIN registered_record AS c ON b.patient_id = c.patient_id) JOIN doctor AS d ON c.doctor_id = d.doctor_id) JOIN department AS e ON d.department_id = e.department_id) JOIN hospital AS f ON e.hospital_id = f.hospital_id WHERE c.record_id = ?";
+    const result = await this.app.mysql.query(sql, id);
     if (result.length > 0) {
-      let temp = utils.removePrefix(result[0]);
-      return { data: temp, status: 0 };
+      return { data: utils.objectUSTC(result[0]), status: 0 };
     } else {
       return { msg: '暂无数据', status: 1 };
     }
